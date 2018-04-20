edff192fa9353832313ffacdfb924b432dace4df
hyunjun529 <hyunjun529@gmail.com>
2018-04-20 18:32:02 +0900

    Let go

--- player/eva2.py
+++ player/eva2.py
@@ -63,27 +63,3 @@ def think(hands, history, old_games):
     return hands[randint(1, len(hands)) - 1]
 
 ##############################################################################
-
-'''
-if __name__ == "__main__":
-    for episode in range(1000):
-        state = env.reset()
-
-        while True:
-            env.render()
-
-            # take action and proceed one step in the environment
-            action = agent.get_action(str(state))
-
-            next_state, reward, done = env.step(action)
-
-            # with sample <s,a,r,s'>, agent learns new q function
-            agent.learn(str(state), action, reward, str(next_state))
-
-            state = next_state
-            env.print_value_all(agent.q_table)
-
-            # if episode ends, then break
-            if done:
-                break
-'''
