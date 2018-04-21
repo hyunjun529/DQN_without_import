8b050891e92a0030328490731c4af0a5466e111e
hyunjun529 <hyunjun529@gmail.com>
2018-04-21 20:47:54 +0900

    setup DQN

--- envFive_eva3.py
+++ envFive_eva3.py
@@ -1,58 +1,75 @@
 import numpy as np
+import tensorflow as tf
+import random
+from collections import deque
 from random import randint
 
-import player.eva0 as eva0
-import player.eva1 as eva1
-import player.eva2 as eva2
+import player.dqn as dqn
 
 
-def envFive(agent, B):
-    history = []
-    hands_A = ['5', '4', '3', '2', '1', '!']
-    hands_B = ['5', '4', '3', '2', '1', '!']
-    old_games = []
+INPUT_SIZE = 12
+OUTPUT_SIZE = 10
+
+DISCOUNT_RATE = 0.99
+REPLAY_MEMORY = 50000
+MAX_EPISODE = 50000000
+BATCH_SIZE = 64
+
+# minimum epsilon for epsilon greedy
+MIN_E = 0.0
+# epsilon will be `MIN_E` at `EPSILON_DECAYING_EPISODE`
+EPSILON_DECAYING_EPISODE = MAX_EPISODE * 0.01
+
+
+class envFive:
+    def __init__(self):
+        self.history = []
+        self.hands_A = ['5', '4', '3', '2', '1', '!']
+        self.hands_B = ['5', '4', '3', '2', '1', '!']
+        self.old_games = []
+
+        self.turn = 1
+        self.score = 0 # A = - , B = +
+        self.winner = 0 # A = -1, B = +1, Draw = 0
+
+        self.record_win = 0
+        self.record_lose = 0
+        self.record_draw = 0
+
+    def reset(self):
+        self.history = []
+        self.hands_A = ['5', '4', '3', '2', '1', '!']
+        self.hands_B = ['5', '4', '3', '2', '1', '!']
+        self.old_games = []
+
+        self.turn = 1
+        self.score = 0 # A = - , B = +
+        self.winner = 0 # A = -1, B = +1, Draw = 0
+
+        self.record_win = 0
+        self.record_lose = 0
+        self.record_draw = 0
+
+        # 첫 손패는 랜덤으로 발생
+        state, reward, done, info = self.step(self.getRandomCardA())
+        return state
     
-    turn = 1
-    score = 0 # A = - , B = +
-    winner = 0 # A = -1, B = +1, Draw = 0
+    def getRandomCardA(self):
+        return randint(0, 9)
 
-    play_A = '?'
-    play_B = '?'
+    def getRandomCardB(self):
+        return self.hands_B[randint(1, len(self.hands_B)) - 1]
 
-    record_win = 0
-    record_lose = 0
-    record_draw = 0
+    def step(self, action):
+        state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        reward = 0
+        done = False
+        info = ["info"]
 
-    isEnd = True
-
-    # rl
-    action = 0
-    action_idx = 0
-    state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    next_state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    reward = 0
-
-    while(isEnd):
-        # 플레이 받기
-        # play_A = A(hands_A, history, old_games)
-        '''
-        if turn == 1:
-            play_A = hands_A[randint(1, len(hands_A)) - 1]
-        '''
-        if turn == 6:
-            play_A = hands_A[0]
-        else:
-            action = agent.get_action(str(state))
-            #print("====================action : " + str(action))
-            action_idx = int(round(len(hands_A) / 10  * (action + 1) - 0.51))
-            play_A = hands_A[action_idx]
-            '''
-            print("====================action : " + str(action))
-            print("====================act_idx : " + str(action_idx))
-            print("====================play_A : " + play_A)
-            '''
-
-        play_B = B(hands_B, history, old_games)
+        action_idx = int(round(len(self.hands_A) / 10  * (action + 1) - 0.51))
+        
+        play_A = self.hands_A[action_idx]
+        play_B = self.getRandomCardB()
 
         # 승패 판정
         winner = 0
@@ -75,85 +92,123 @@ def envFive(agent, B):
             return -1
 
         # 승부 기록 및 다음 게임 준비
-        score += winner
-        hands_A.remove(play_A)
-        hands_B.remove(play_B)
-        history.append([play_A, play_B])
+        self.score += winner
+        self.hands_A.remove(play_A)
+        self.hands_B.remove(play_B)
+        self.history.append([play_A, play_B])
+
+        # reward : 이겼으면 1, 졌으면 -1,
+        reward = -winner
 
         # 승부 결과 출력 (A를 기준으로 승, 패)
         display = '?'
         if winner == 0:
             display = '무'
-            record_draw += 1
+            self.record_draw += 1
         elif winner < 0:
             display = '승'
-            record_win += 1
+            self.record_win += 1
         elif winner > 0:
             display = '패'
-            record_lose += 1
-        #print(str(turn) + '턴 : ' + play_A + ", " + play_B + " : " + display)        
+            self.record_lose += 1
+        # print(str(self.turn) + '턴 : ' + play_A + ", " + play_B + " : " + display + "reward : " + str(reward))
 
-         # next_state : 현재 history로 갱신
-        his_boku = [row[0] for row in history]
-        his_teki = [row[1] for row in history]
+        # next_state : 현재 history로 갱신
+        his_boku = [row[0] for row in self.history]
+        his_teki = [row[1] for row in self.history]
         cnt = 1
         for i in his_boku:
             tmp_idx = 0
             if i != '!':
                 tmp_idx = int(i)
-            next_state[tmp_idx] = cnt
+            state[tmp_idx] = cnt
             cnt += 1
         cnt = 1
         for i in his_teki:
             tmp_idx = 0
             if i != '!':
                 tmp_idx = int(i)
-            next_state[tmp_idx + 6] = cnt
+            state[tmp_idx + 6] = cnt
             cnt += 1
-        '''
-        print("====================next_state : " + str(next_state))
-        '''
-
-        # reward : 이겼으면 1, 졌으면 -1,
-        reward = -winner
-
-        # learn : 지식이 늘었다
-        '''
-        print("====================state : " + str(state))
-        print("====================action : " + str(action) + " (realplay : " + play_A + ")")
-        print("====================reward : " + str(reward))
-        print("====================next_state : " + str(next_state))
-        '''
-        agent.learn(str(state), action, reward, str(next_state))
-        state = next_state
 
         # 게임 종료
-        turn += 1
-        if abs(score) >= 3 or turn >= 7:
-            isEnd = False
+        self.turn += 1
+        if abs(self.score) >= 3 or self.turn >= 7:
+            done = True
 
-    # 게임 결과
-    '''
-    print("A는 \""\
-        + str(record_win) + "승 "\
-        + str(record_lose) + "패 "\
-        + str(record_draw) + "무\" 를 기록했다")
-    '''
-    #print("경기 기록 : " + str(history))
+        return state, reward, done, info
 
-    return (record_win - record_lose)
+
+def annealing_epsilon(episode: int, min_e: float, max_e: float, target_episode: int) -> float:
+    slope = (min_e - max_e) / (target_episode)
+    intercept = max_e
+
+    return max(min_e, slope * episode + intercept)
+
+
+def train_minibatch(DQN: dqn.DQN, train_batch: list) -> float:
+    state_array = np.vstack([x[0] for x in train_batch])
+    action_array = np.array([x[1] for x in train_batch])
+    reward_array = np.array([x[2] for x in train_batch])
+    next_state_array = np.vstack([x[3] for x in train_batch])
+    done_array = np.array([x[4] for x in train_batch])
+
+    X_batch = state_array
+    y_batch = DQN.predict(state_array)
+
+    Q_target = reward_array + DISCOUNT_RATE * np.max(DQN.predict(next_state_array), axis=1) * ~done_array
+    y_batch[np.arange(len(X_batch)), action_array] = Q_target
+
+    # Train our network using target and predicted Q values on each episode
+    loss, _ = DQN.update(X_batch, y_batch)
+
+    return loss
+
+
+env = envFive()
 
 
 if __name__ == '__main__':
-    agent = eva2.QLearningAgent()
-    memory = 0
-    savepoint = 0
-    rate = 1000000
-    while(True):
-        memory += envFive(agent, eva1.think)
-        if savepoint == rate:
-            agent.save_q_table()
-            print(memory/rate) # 승률
-            memory = 0
-            savepoint = 0
-        savepoint+=1
+    print("HELL WORLD")
+    replay_buffer = deque(maxlen=REPLAY_MEMORY)
+    last_100_game_reward = deque(maxlen=100)
+    
+    with tf.Session() as sess:
+        mainDQN = dqn.DQN(sess, INPUT_SIZE, OUTPUT_SIZE)
+        init = tf.global_variables_initializer()
+        sess.run(init)
+
+        for episode in range(MAX_EPISODE):
+            e = annealing_epsilon(episode, MIN_E, 1.0, EPSILON_DECAYING_EPISODE)
+            done = False
+            state = env.reset()
+
+            step_count = 0
+            while not done:
+
+                if np.random.rand() < e:
+                    action = env.getRandomCardA()
+                else:
+                    action = np.argmax(mainDQN.predict(state))
+
+                next_state, reward, done, _ = env.step(action)
+
+                if done:
+                    reward = -1
+
+                replay_buffer.append((state, action, reward, next_state, done))
+
+                state = next_state
+                step_count += 1
+
+                if len(replay_buffer) > BATCH_SIZE:
+                    minibatch = random.sample(replay_buffer, BATCH_SIZE)
+                    train_minibatch(mainDQN, minibatch)
+
+            # print("[Episode {:>5}]  steps: {:>5} e: {:>5.2f}".format(episode, step_count, e))
+
+            last_100_game_reward.append(step_count)
+            if len(last_100_game_reward) == last_100_game_reward.maxlen:
+                avg_reward = np.mean(last_100_game_reward)
+                if avg_reward > 4.5:
+                    print("Reached goal within {} episodes with avg reward {}".format(episode, avg_reward))
--- player/eva3.py
+++ player/eva3.py
@@ -1,3 +1,5 @@
+import player.dqn
+
 from random import randint
 
 ### random agent
@@ -6,9 +8,9 @@ def think(hands, history, old_games):
 
 ##############################################################################
 
+
+
+##############################################################################
+
 if __name__ == '__main__':
-    hands_all = ['5', '4', '3', '2', '1', '!']
-    history = [['5', '2'], ['4', '3'], ['3', '4'], ['1', '5']]
-    hands = hands_all
-    old_games = []
-    print(think(hands, history, old_games))
+   print("HELL WORLD")
